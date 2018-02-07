4fa4894bcd2db2315dced37c1b5a7c6bb587dbd8
Rosie Wang <rosiedwang@yahoo.com>
2018-02-07 21:22:27 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,12 +1,13 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init(){
-  const body = document.getElementByTagName('body');
+  const body = document.getElementsByTagName('body');
   body.addEventListener('keydown',onKeyDownHandler(e));
 }
 
 function onKeyDownHandler(e) {
-  const key = parseInt(e.detail || e.which);
+  const key = parseInt(e.which||e.detail);
+  var index =0;
   if(key===code[index])
   {
       index++;
